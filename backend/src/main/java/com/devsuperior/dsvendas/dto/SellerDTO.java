3e49c4b004d60de679fab28ec098219cbae9bf4b
Paulo Honorato <paulohonoratos@hotmail.com>
2021-06-08 17:52:06 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import com.devsuperior.dsvendas.entities.Seller;
 
 public class SellerDTO implements Serializable {
-    // private static final long serialVersionUID = 1l;
 
     private Long id;
     private String name;
